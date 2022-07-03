888862d000a18458e480955d42a0a27932d0f863
halimyusuf <haleemyoosuph@gmail.com>
2022-07-03 01:32:15 +0100

    chore: added url to links

--- layout/navbar.tsx
+++ layout/navbar.tsx
@@ -1,17 +1,26 @@
-import { Box, Container, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <Box padding={{ base: "0.5rem 2rem", md: "1rem 3rem" }}>
+    <Box padding={{ base: "0.5rem 1rem", md: "1rem 3rem" }}>
       <Flex justifyContent="space-between" align="center">
         <Box>
-          <Text fontSize={{ base: "24", md: "24" }} fontWeight="bold">
+          <Text fontSize={{ base: "20", md: "24" }} fontWeight="bold">
             HALIM.Y.
           </Text>
         </Box>
         <Flex>
-          <Box padding="2">Projects</Box>
-          <Box padding="2">About</Box>
+          <Link href="/projects">
+            <Button colorScheme="white" variant="link" p="2">
+              Projects
+            </Button>
+          </Link>
+          <Link href="/about">
+            <Button colorScheme="white" variant="link" p="2">
+              About
+            </Button>
+          </Link>
         </Flex>
       </Flex>
     </Box>
